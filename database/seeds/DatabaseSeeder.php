132c0f4a8e2393ac5adbbb977994de8a37dd6d29
zTin <quachtothinh@gmal.com>
2017-09-29 16:50:08 +0700

    seeder them du lieu mau bang seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +13,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+       /* DB::table('users')->insert([
+        	'name' => 'Thinh',
+        	'email' => "quachth@gmail.com",
+        	'password' => bcrypt('matkhau')
+
+        	//dung lenh php artisan db:seed de chay trong cmd
+        ]);*/
+        $this->call(userSeeder::class);
+    }
+}
+
+class userSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([            
+            ['name' => 'PHP','email' => str_random(3).'@gmail.com','password' => bcrypt('matkhau')],
+            ['name' => 'KhoaPham','email' => str_random(3).'@gmail.com','password' => bcrypt('matkhau')],
+            ['name' => 'Laravel','email' => str_random(3).'@gmail.com','password' => bcrypt('matkhau')],
+        ]);
     }
 }
